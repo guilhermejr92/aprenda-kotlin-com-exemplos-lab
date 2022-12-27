fef73cec6d897e970a573cf76a981084d3f98b01
Guilherme Junior <106559257+guilhermejr92@users.noreply.github.com>
2022-12-27 11:41:38 -0300

    finalizando

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,57 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel {Básico, Intermediário, Avançado}
 
-enum class Nivel { EASY, MEDIUM, HARD }
+data class Usuario(val level: String, val skill: String, val vocação: String)
 
-class User
+data class Planilha&Tutorial(val level: String, val duracao: Int, val nivel: Calculator)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Hunts(val nome: String, val dicas: List<MelhoresLocaisdeHunt>, val nivel: Calculator) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<User>()
+    val classes = mutableListOf<Usuario>()
     
-    fun matricular(usuario: User) {
-        TODO("Utilize o parâmetro $user para simular uma matrícula (usar a lista de $inscritos).")
+    fun cadastrar(usuario: Usuario) {
+        cadastrar.add(usuario)
+    }
+    
+    fun imprimirListaCadastrar(){
+        for(user in cadastrar){
+            println(user.level)
+        }
+    }
+    
+    fun imprimirGradePlanilha&Tutorial(){
+        for(cont in dicas){
+            println("${cont.level} - ${cont.duracao}")
+        }
     }
 }
 
 fun main() {
-    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val PlanilhaKnight = MelhoresLocaisdeHunt ("Do 0 ao 1000", 70, Nivel.Intermediário)
+    val PlanilhaPaladin = MelhoresLocaisdeHunt("Do 0 ao 1000", 80, Nivel.Intermediário)
+    val PlanilhaDruid = MelhoresLocaisdeHunt("Do 0 ao 1000", 90, Nivel.Avançado)
+    val PlanilhaSorcerer = MelhoresLocaisdeHunt("Do 0 ao 1000", 90, Nivel.Avançado)
+    
+    val listMelhoresLocaisdeHunt = mutableListOf<Usuario>()
+    listMelhoresLocaisdeHunt.add(PlanilhaKnight)
+    listMelhoresLocaisdeHunt.add(PlanilhaPaladin)
+    listMelhoresLocaisdeHunt.add(PlanilhaMage)
+    listMelhoresLocaisdeHunt.add(PlanilhaSorcerer)
+    
+    val Calculator = Vocação("Tutorial Level e Skill", listMelhoresLocaisdeHunt, Nivel.Avançado)
+    
+    val Knight = Usuario("Char", "knight@zmail.com", "premium")
+    val Paladin = Usuario("Char", "paladin@zmail.com", "premium")
+    val Druid = Usuario("Char", "druid@zmail.com", "premium")
+    val Sorcerer = Usuario("Char", "sorcerer@zmail.com", "premium")
+    
+    Calculator.cadastrar()
+    Calculator.cadastrar()
+    
+    print("Calculator: ")
+    println("${Calculator.level} - ${Calculator.nivel}")
+    println(Calculator.imprimirGradeVocação())
+    println()
+    println("Opções de hunts: ")
+    println(Calculator.imprimirOpçõesdehunts())
 }
